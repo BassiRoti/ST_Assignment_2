f988162aca353a745e4d4e2dd87db386c8669613
Abbas Rizvi <abbasriz1234567890@gmail.com>
2024-11-29 19:37:55 +0500

    bfr

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -7,6 +7,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+//print
+
 public class LoginApp extends JFrame {
     private JTextField emailField;
     private JPasswordField passwordField;
